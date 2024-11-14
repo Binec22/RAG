de88562d4a42fb82d3d14d9f0faf2cf829ea42c2
Antonin Larvor <antonin226@gmail.com>
2024-11-14 19:26:53 +0200

    First Version

---
+++ python_script/parameters.py
@@ -0,0 +1,375 @@
+import json
+import os
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+
+DATA_PATH = None
+CHROMA_ROOT_PATH = None
+EMBEDDING_MODEL = None
+LLM_MODEL = None
+PROMPT_TEMPLATE = None
+REPHRASING_PROMPT = None
+STANDALONE_PROMPT = None
+ROUTER_DECISION_PROMPT = None
+
+
+def load_api_keys():
+    """
+    Load API keys from the .env file and set them as environment variables.
+    """
+    load_dotenv()
+    os.environ["HF_API_TOKEN"] = os.getenv("HF_API_TOKEN")
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    os.environ["VOYAGE_API_KEY"] = os.getenv("VOYAGE_API_KEY")
+
+
+def load_config(config_name='default', show_config=False):
+    """
+    Load configuration settings from a JSON file based on the provided configuration name.
+    This function sets global variables for various configuration parameters.
+
+    JSON Structure:
+    {
+      "config_name": {
+        "data_path": "",        # Path to the data folder
+        "chroma_root_path": "", # Path to the folder where the Chroma database will be stored
+        "embedding_model": "",  # Model to use for embeddings (e.g., 'sentence-transformers/all-mpnet-base-v2', 'openai', 'voyage-law-2')
+        "llm_model": "",        # Model to use for the language model (e.g., 'gpt-3.5-turbo', 'mistralai/Mistral-7B-Instruct-v0.1', 'nvidia/Llama3-ChatQA-1.5-8B')
+        "prompt_template": ""   # Template for the prompt
+      }
+    }
+    
+    Ready to use models are here. For other model you will maybe need to modify code
+    
+    Embeddings models : - "sentence-transformers/all-mpnet-base-v2"
+                        - "openai"
+                        - "voyage-law-2"
+    LLM models :    - "gpt-3.5-turbo"
+                    - "mistralai/Mistral-7B-Instruct-v0.1"
+                    - "mistralai/Mixtral-8x7B-Instruct-v0.1"
+                    - "nvidia/Llama3-ChatQA-1.5-8B"                        
+    """
+    global DATA_PATH, CHROMA_ROOT_PATH, EMBEDDING_MODEL, LLM_MODEL
+    try:
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        try:
+            with open('python_script/config.json', 'r') as file:
+                config = json.load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError("The configuration file cannot be found in the specified paths.")
+    except json.JSONDecodeError:
+        raise ValueError("The configuration file is present but contains a JSON format error.")
+
+    selected_config = config[config_name]
+
+    DATA_PATH = selected_config['data_path']
+    CHROMA_ROOT_PATH = selected_config['chroma_root_path']
+    EMBEDDING_MODEL = selected_config['embedding_model']
+    LLM_MODEL = selected_config['llm_model']
+
+    load_prompt(config_name)
+    load_api_keys()
+
+    if show_config:
+        print_config()
+
+
+def print_config():
+    """
+    Print the current configuration settings.
+    This function prints the values of the global configuration parameters.
+    """
+    global DATA_PATH, CHROMA_ROOT_PATH, EMBEDDING_MODEL, LLM_MODEL
+
+    print("\nCurrent Configuration Settings:\n")
+    print(f"Data Path: {DATA_PATH}")
+    print(f"Chroma Root Path: {CHROMA_ROOT_PATH}")
+    print(f"Embedding Model: {EMBEDDING_MODEL}")
+    print(f"Language Model: {LLM_MODEL}\n")
+
+
+def load_prompt(config_name):
+    """
+    Load the different prompts used in the conversational rag chain
+    """
+    global PROMPT_TEMPLATE, REPHRASING_PROMPT, STANDALONE_PROMPT, ROUTER_DECISION_PROMPT
+    if config_name == 'arch_ru':
+        STANDALONE_PROMPT = PromptTemplate.from_template("""# Context #
+        This is part of a conversational retrieval-augmented generator (RAG) AI system to answer user questions. 
+
+        #########
+
+        # Objective #
+        Take the original user question and chat history, and generate a new standalone question in russian that can be understood and answered without relying on additional external information.
+
+        #########
+
+        # Style #
+        The reshaped standalone question should be clear, concise, and self-contained, while maintaining the intent and meaning of the original query.
+
+        #########
+
+        # Tone #
+        Neutral and focused on accurately capturing the essence of the original question.
+
+        #########
+
+        # Audience #
+        The audience is the internal system components that will act on the decision.
+
+        #########
+
+        # Response #
+        If the original question requires reshaping, provide a new reshaped standalone question in russian that includes all necessary context and information to be self-contained.
+        If no reshaping is required, simply output the original question as is in russian.
+
+        ##################
+
+        # Chat History #
+        {chat_history}
+
+        #########
+
+        # User original question #
+        {question}
+
+        #########
+
+        # The new Standalone question in russian#<|endofprompt|>""")
+
+        PROMPT_TEMPLATE = PromptTemplate.from_template("""# Context #
+        This is part of a conversational retrieval-augmented generator (RAG) AI system to answer user questions about russian documents. 
+
+        #########
+
+        # Objective #
+        Generate an appropriate response in English to the user's question based only on the retieved context in Russian and the chat history in English.
+
+        #########
+
+        # Style #
+        The response should be as detailed as the context allows and follow the order of the conversation.
+
+        #########
+
+        # Tone #
+        Analytical and objective.
+
+        #########
+
+        # Audience #
+        The audience is the user that ask the question.
+
+        #########
+
+        # Response #
+        Provide a detailled awnser in English that is relevant to the context given.
+
+        ##################
+
+        # User question #
+        {question}
+
+        #########
+
+        # Chat history #
+        {chat_history}
+
+        #########
+
+        # Context retrieved #
+        {context}
+
+        #########
+
+        # Your Awnser in English Here #<|endofprompt|>""")
+    else:
+        PPROMPT_TEMPLATE = PromptTemplate.from_template("""# Context #
+        This is part of a conversational retrieval-augmented generator (RAG) AI system to answer user questions. 
+
+        #########
+
+        # Objective #
+        Generate an appropriate response to the user's question based only on the retieved context and the chat history.
+
+        #########
+
+        # Style #
+        The response should be as detailed as the context allows and follow the order of the conversation.
+
+        #########
+
+        # Tone #
+        Analytical and objective.
+
+        #########
+
+        # Audience #
+        The audience is the user that ask the question.
+
+        #########
+
+        # Response #
+        Provide a detailled awnser that is relevant to the context given.
+
+        ##################
+
+        # User question #
+        {question}
+
+        #########
+
+        # Chat history #
+        {chat_history}
+
+        #########
+
+        # Context retrieved #
+        {context}
+
+        #########
+
+        # Your Awnser Here #<|endofprompt|>""")
+
+        STANDALONE_PROMPT = PromptTemplate.from_template("""# Context #
+        This is part of a conversational retrieval-augmented generator (RAG) AI system to answer user questions. 
+
+        #########
+
+        # Objective #
+        Take the original user question and chat history, and generate a new standalone question that can be understood and answered without relying on additional external information.
+
+        #########
+
+        # Style #
+        The reshaped standalone question should be clear, concise, and self-contained, while maintaining the intent and meaning of the original query.
+
+        #########
+
+        # Tone #
+        Neutral and focused on accurately capturing the essence of the original question.
+
+        #########
+
+        # Audience #
+        The audience is the internal system components that will act on the decision.
+
+        #########
+
+        # Response #
+        If the original question requires reshaping, provide a new reshaped standalone question that includes all necessary context and information to be self-contained.
+        If no reshaping is required, simply output the original question as is.
+
+        ##################
+
+        # Chat History #
+        {chat_history}
+
+        #########
+
+        # User original question #
+        {question}
+
+        #########
+
+        # The new Standalone question #<|endofprompt|>""")
+
+    ROUTER_DECISION_PROMPT = PromptTemplate.from_template("""# Context #
+    This is part of a conversational AI system that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+
+    #########
+
+    # Objective #
+    Evaluate the given question and decide whether the RAG application is required to provide a comprehensive answer by retrieving relevant information from a knowledge base, or if the chat model's inherent knowledge is sufficient to generate an appropriate response.
+
+    #########
+
+    # Style #
+    The response should be a clear and direct decision, stated concisely.
+
+    #########
+
+    # Tone #
+    Analytical and objective.
+
+    #########
+
+    # Audience #
+    The audience is the internal system components that will act on the decision.
+
+    #########
+
+    # Response #
+    If the question should be rephrased return response in YAML file format:
+    ```
+        result: true
+    ```
+    otherwise return in YAML file format:
+    ```
+        result: false
+    ```
+
+    ##################
+
+    # Chat History #
+    {chat_history}
+
+    #########
+
+    # User question #
+    {question}
+
+    #########
+
+    # Your Decision in YAML format #<|endofprompt|>""")
+
+    REPHRASING_PROMPT = PromptTemplate.from_template("""# Context #
+    This is part of a conversational retrieval-augmented generator (RAG) AI system to answer user questions. 
+
+    #########
+
+    # Objective #
+    Evaluate the given user question and determine if it requires reshaping according to chat history to provide necessary context and information for answering, or if it can be processed as is.
+    Rephrasing is required if the user's question is ambiguous without the context of the previous conversation, or if specific information from the chat history is essential to understand or correctly answer the question.
+    #########
+
+    # Style #
+    The response should be clear, concise, and in the form of a straightforward decision - either "Reshape required" or "No reshaping required".
+
+    #########
+
+    # Tone #
+    Professional and analytical.
+
+    #########
+
+    # Audience #
+    The audience is the internal system components that will act on the decision.
+
+    #########
+
+    # Response #
+    If the question should be rephrased return response in YAML file format:
+    ```
+        result: true
+    ```
+    otherwise return in YAML file format:
+    ```
+        result: false
+    ```
+
+    ##################
+
+    # Chat History #
+    {chat_history}
+
+    #########
+
+    # User question #
+    {question}
+
+    #########
+
+    # Your Decision in YAML format #<|endofprompt|>""")
---
+++ python_script/populate_database.py
@@ -0,0 +1,260 @@
+import argparse
+import os
+import shutil
+
+from tqdm import tqdm
+from langchain.schema.document import Document
+
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PDFPlumberLoader
+
+from typing import List, Union
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    parser = argparse.ArgumentParser(description="This script manages the database for the application. You can use it to clear the database, reset it, or populate it with a specific configuration that can be specified in the config.json file.")
+    parser.add_argument("--config", type=str, help="Enter the name of the config you want to populate your database.")
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    parser.add_argument("--clear", action="store_true", help="Clear the database.")
+
+    args = parser.parse_args()
+
+    if args.clear:
+        print("✨ Clearing Database")
+        if args.config:
+            # Clear the specific subfolder specified in the config
+            load_config(args.config)
+            subfolder_name = "chroma_{}".format(EMBEDDING_MODEL)
+            clear_database(subfolder_name)
+        else:
+            # Clear the whole database
+            clear_database()
+        return
+
+    if args.config:
+        load_config(args.config)
+        if args.reset:
+            # Clear the existing database existing for the specified config
+            subfolder_name = "chroma_{}".format(EMBEDDING_MODEL)
+            print("✨ Reseting Database")
+            try:
+                clear_database(subfolder_name)
+            except FolderNotFoundError as e:
+                print(e)
+
+        # Create (or update) the data store.
+        documents = load_documents()
+        chunks = split_documents(documents)
+        add_to_chroma(chunks)
+
+def load_config(config_name):
+    """
+    Load and print the parameters entered for config_name into the config.json file.
+    """
+    global DATA_PATH, CHROMA_ROOT_PATH, EMBEDDING_MODEL, LLM_MODEL, PROMPT_TEMPLATE
+    from parameters import load_config as ld
+    ld(config_name, show_config=True)
+    from parameters import DATA_PATH, CHROMA_ROOT_PATH, EMBEDDING_MODEL, LLM_MODEL, PROMPT_TEMPLATE   
+
+def load_documents():
+    """
+    Load pdf documents with a langchain tool
+    Load txt and docx with a llamaindex tool
+    Convert everything in the same Document format
+
+    #TODO Make something better maybe
+    #TODO Implement other document type. llamaindex tool can do it
+    """
+    from llama_index.core import SimpleDirectoryReader
+
+    langchain_documents = []
+    llama_documents = []
+
+    try:
+        llama_document_loader = SimpleDirectoryReader(input_dir=DATA_PATH, required_exts=[".txt", ".docx"])
+        for doc in tqdm(llama_document_loader.load_data(), desc="TXT/DOCX loaded"):
+            doc.metadata.pop('file_path',None)
+            print(doc.metadata)
+            llama_documents.append(doc)
+    except ValueError as e:
+        print(e)
+
+    langchain_document_loader = ProgressPyPDFDirectoryLoader(DATA_PATH)
+    for doc in tqdm(langchain_document_loader.load(), desc="PDFs loaded"):
+        print(doc.metadata)
+        langchain_documents.append(doc)
+
+    documents = langchain_documents + convert_llamaindexdoc_to_langchaindoc(llama_documents)
+    print(f"Loaded {len(langchain_documents)} chunks from PDF documents, {len(llama_documents)} chunks from TXT/DOCX documents.\nTotal chunks: {len(documents)}.\n")
+    return documents
+
+def convert_llamaindexdoc_to_langchaindoc(documents: list[Document]):
+    """
+    Convert documents from llamaindex library into the same format as those in langchain, so that you can create a single list
+    """
+    langchain_docs = []
+    for doc in documents:
+        langchain_docs.append(Document(page_content=doc.text, metadata=doc.metadata))
+    return langchain_docs
+
+def split_documents(documents: list[Document]):
+    """
+    Split documents into smaller chunks
+    """
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def add_to_chroma(chunks: list[Document]):
+    """
+    Load the chroma database
+    Check if there are new documents in the documents database
+    Add them to the chroma database
+    """
+    from langchain_community.vectorstores import Chroma
+    from get_embedding_function import get_embedding_function
+    # Load the existing database.
+    db = Chroma(
+        persist_directory=find_chroma_path(EMBEDDING_MODEL,CHROMA_ROOT_PATH), embedding_function=get_embedding_function(EMBEDDING_MODEL)
+    )
+
+    # Calculate Page IDs.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # Add or Update the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # Only add documents that don't exist in the DB.
+    new_chunks = [chunk for chunk in chunks_with_ids if chunk.metadata["id"] not in existing_ids]
+    batch_size = 1000
+
+    if new_chunks:
+        with tqdm(total=len(new_chunks), desc="Adding documents") as pbar:
+            for i in range(0,len(new_chunks), batch_size):
+                batch = new_chunks[i:i + batch_size]
+                new_chunk_ids = [chunk.metadata["id"] for chunk in batch]
+                db.add_documents(batch, ids=new_chunk_ids)
+                db.persist()
+                pbar.update(len(batch))
+
+    else:
+        print("✅ No new documents to add")
+
+
+def calculate_chunk_ids(chunks):
+    """
+    Add metadata id to the chunk in the following format:
+    document_name.extension:page_number:chunk_number
+    """
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("file_name")
+        page = str(chunk.metadata.get("page"))
+        current_page_id = f"{source}:p{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:c{current_chunk_index}"
+        last_page_id = current_page_id
+        chunk.metadata["id"] = chunk_id
+    return chunks
+
+def find_chroma_path(model_name, base_path):
+    """
+    Find the path to the chroma database corresponding to the Embedding model
+    Create the subfolder in the chroma root folder if not exists
+    """
+    if not model_name:
+        raise ValueError("Model name can't be empty")
+
+    if not base_path:
+        try:
+            base_path = CHROMA_ROOT_PATH
+        except:
+            raise ValueError("The Chroma database root file is not populated")
+    # Set model_path
+    model_path = os.path.join(base_path, f"chroma_{model_name}")
+    if not os.path.exists(model_path):
+        # Create folder if doesn't exist
+        os.makedirs(model_path)
+    return model_path
+
+def clear_database(chroma_subfolder_name = None):
+    """
+    Clear the folder if chroma_subfolder_name is set and exists
+    Clear the whole database if not set but ask the user before
+    """
+    if chroma_subfolder_name:
+        full_path = os.path.join(CHROMA_ROOT_PATH, chroma_subfolder_name)
+        if os.path.exists(full_path):
+            shutil.rmtree(full_path)
+            print(f"The database in {full_path} has been successfully deleted.")
+        else:
+            raise FolderNotFoundError(f"Folder {full_path} doesn't exist")
+    else:
+        print("\nExisting databases :\n\n")
+        subfolders = [f for f in os.listdir(CHROMA_ROOT_PATH) if os.path.isdir(os.path.join(CHROMA_ROOT_PATH, f))]
+        if not subfolders:
+            print(f"no subfolder found in {CHROMA_ROOT_PATH}\n\n")
+        for subfolder in subfolders:
+            print(f"- {subfolder}")
+        
+        confirmation = input("Do you want to delete all the databases ? (yes/no) : ")
+        if confirmation.lower() == 'yes':
+            shutil.rmtree(CHROMA_ROOT_PATH)
+            print("All databases cleared")
+        else:
+            print("Deletion cancelled.")
+
+
+class ProgressPyPDFDirectoryLoader(PyPDFDirectoryLoader):
+    def load(self) -> List[Document]:
+        p = Path(self.path)
+        docs = []
+        items = list(p.rglob(self.glob)) if self.recursive else list(p.glob(self.glob))
+        
+        with tqdm(total=len(items), desc="Loading PDFs") as pbar:
+            for i in items:
+                if i.is_file():
+                    if self._is_visible(i.relative_to(p)) or self.load_hidden:
+                        try:
+                            loader = PDFPlumberLoader(str(i), extract_images=self.extract_images)
+                            sub_docs = loader.load()
+                            for doc in sub_docs:
+                                if 'source' in doc.metadata:
+                                    doc.metadata['source'] = i.name
+                                    doc.metadata['file_name'] = doc.metadata.pop('source')
+                                doc.metadata.pop('file_path',None)
+                                doc.metadata = {key: value for key, value in doc.metadata.items() if value}
+                            docs.extend(sub_docs)
+                        except Exception as e:
+                            if self.silent_errors:
+                                logger.warning(e)
+                            else:
+                                raise e
+                pbar.update(1)
+        return docs
+
+class FolderNotFoundError(Exception):
+    """Exception raised when a folder is not found."""
+    pass
+
+if __name__ == "__main__":
+    main()
